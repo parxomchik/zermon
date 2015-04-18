8e706b4b58736fc93b4db89e2b6d26d94a91510b
artemmmon <thumanazar@yandex.ru>
2015-04-18 13:45:31 +0300

    working at filter

--- js/filter_page.js
+++ js/filter_page.js
@@ -10,7 +10,31 @@ $(document).ready(function() {
     	$('.check_box').removeClass('active');
   	});
 
+    //select made in sm sklipav
 
+    $('.item_sort').click(function() {
+    	$(this).children('.dropdown_sort').toggleClass('hidden');
+    	$(this).children('.dropdown_sort').toggleClass('active');
+    	// $('.item_sort .active_sort').css('border-bottom','1px solid #B47F31');	
+    });
+    $('.item_sort .dropdown_sort li a').click(function() {
+    	$('.item_sort .dropdown_sort li a').removeClass('active');
+    	$('.item_sort').children('.dropdown_sort').removeClass('active');
+    	$(this).addClass('active');
+    	var value = $(this).text();
+    	$('#current_value').text(value);
+    });
+    // $('body:not(.item_sort)').click(function() {
+    // 	$('.item_sort').children('.dropdown_sort').addClass('hidden');
+    // });
+// $(function(){
+//   $('#ex2').slider({
+//        formater: function(value) {
+//          return 'Current value: '+value;
+//        }
+//   }).on('slideStop', function(ev){
+//      $(this).val($(this).data('slider').getValue());
+//   });
 
 // var start = $('#ex2').slider()
 //     .on('slide', inputChange)
